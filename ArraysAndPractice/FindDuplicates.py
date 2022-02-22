4bd6ac01641dca541d6826c63cb1c92f19e2d09e
Hirwa <hirwanshutiflx@gmail.com>
2022-02-22 22:53:43 +0530

    Added Finding duplicates in a sorted and an unsorted list of elements

---
+++ ArraysAndPractice/FindDuplicates.py
@@ -0,0 +1,41 @@
+"""
+Author: Felix
+-------
+Date: 22 - February - 2022
+----
+Last Modified: 22 - February - 2022
+--------------
+
+Finding Duplicates in both unsorted list and sorted list.
+
+"""
+
+
+class Duplicates:
+    def __init__(self, lst: list):
+        """
+        Initializing our list
+        :param lst: A list containing elements of array
+        """
+        self.lst = lst
+        self.length = len(self.lst)
+
+    def findDuplicates(self) -> None:
+        max_el = max(self.lst) + 1
+        helper = [0] * max_el
+        for i in range(self.length):
+            helper[self.lst[i]] += 1
+        for i in range(max_el):
+            if helper[i] > 1:
+                print(f"Element {i} is present {helper[i]} times")
+
+
+if __name__ == "__main__":
+    data_unsort = [8, 3, 6, 4, 6, 5, 6, 8, 2, 7]
+    sorted_arr = [1, 1, 1, 3, 5, 6, 9, 9, 9, 10, 15, 15, 17]
+    print("Duplicates in an unsorted array")
+    array_data = Duplicates(data_unsort)
+    array_data.findDuplicates()
+    print("Duplicates in a sorted array")
+    arr_d = Duplicates(sorted_arr)
+    arr_d.findDuplicates()
