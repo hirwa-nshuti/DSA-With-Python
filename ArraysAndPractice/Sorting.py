47470920fe042fd0edaedbadbc3a7701e6dfcdb0
Hirwa <hirwanshutiflx@gmail.com>
2022-02-23 15:21:21 +0530

    Added Selection sort

--- ArraysAndPractice/Sorting.py
+++ ArraysAndPractice/Sorting.py
@@ -9,8 +9,10 @@ Last Modified: 23 - February - 2022
 Sorting an unsorted sequence using different Algorithms:
 
 1. Bubble Sorting
+2. Selection sorting
 
 """
+
 
 from typing import List
 
@@ -33,12 +35,27 @@ class Sorting:
             if is_sorted == False:
                 break
 
+    def selection_sort(self):
+        for i in range(self.length - 1):
+            small_ndx = i
+            for j in range(i + 1, self.length):
+                if self.seq[j] < self.seq[small_ndx]:
+                    small_ndx = j
+            if small_ndx != i:
+                self.seq[i], self.seq[small_ndx] = self.seq[small_ndx], self.seq[i]
+
 
 if __name__ == "__main__":
-    arr = [10, 2, 18, 4, 31, 13, 5, 23, 51, 29, 64]
-    to_sort = Sorting(arr)
+    to_sort1 = Sorting([10, 2, 18, 4, 31, 13, 15, 23, 51, 29, 64])
     print("Before Bubble Sort")
-    to_sort.Display()
+    to_sort1.Display()
     print("After Bubble Sort")
-    to_sort.bubble_sort()
-    to_sort.Display()
+    to_sort1.bubble_sort()
+    to_sort1.Display()
+    
+    to_sort2 = Sorting([10, 2, 5, 18, 4, 31, 13, 15, 23, 51, 29, 64])
+    print("\nBefore Selection sort")
+    to_sort2.Display()
+    print("After Selection Sort")
+    to_sort2.selection_sort()
+    to_sort2.Display()
