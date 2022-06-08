0ad92e7de0aa50cf38b7421ae17de2aa72384dea
hirwa <hirwanshutiflx@gmail.com>
2022-06-08 22:43:10 +0530

    Added Selection Sort

---
+++ Sorting/slection_sort.py
@@ -0,0 +1,15 @@
+def selection_sort(lst):
+    size = len(lst)
+    for i in range(size-1):
+        min_index = i
+        for j in range(min_index+1, size):
+            if lst[j] < lst[min_index]:
+                min_index = j
+        if i != min_index:
+            lst[i], lst[min_index] = lst[min_index], lst[i]
+
+
+if __name__ == "__main__":
+    arr=[3, 2, 0, 9, 12, 32, 23, 10]
+    selection_sort(arr)
+    print(arr)
