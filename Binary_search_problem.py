9cc7b4ccb59c86e49fa58de88563c01fcaa27335
hirwa <hirwanshutiflx@gmail.com>
2022-07-16 16:15:19 +0530

    Added test cases and description to bin_search

---
+++ Binary_search_problem.py
@@ -0,0 +1,49 @@
+"""
+**QUESTION:** Alice has some cards with numbers written on them. She arranges the cards in decreasing order,
+and lays them out face down in a sequence on a table. She challenges Bob to pick out the card containing a given
+number by turning over as few cards as possible. Write a function to help Bob locate the card.
+
+Typical approach to the problem
+-------------------------------
+**Problem statement:** We have a task to search for an element in a decreasing ordered list and return its index.
+
+**Inputs:** The sorted sequence ex: [10, 5, 3, 2 , 1], and the variable to search ex: 3.
+**Outputs:** The position of searched variable ex: 2 if not present the function will return 0
+"""
+
+test_cases = [
+    {
+        'inputs': {
+            'cards': [10, 8, 5, 4, 2, 1],
+            'query': 5, },
+        'output': 3
+    },
+    {
+        'inputs': {
+            'cards': [8, 6, 2, 1],
+            'query': 8, },
+        'output': 0
+    },
+    {
+        'inputs': {
+            'cards': [],
+            'query': 5, },
+        'output': -1
+    },
+    {
+        'inputs': {
+            'cards': [10, 9, 5, 3, 2, 1],
+            'query': 1, },
+        'output': 5
+    },
+    {
+        'inputs': {
+            'cards': [10],
+            'query': 7, },
+        'output': -1
+    },
+]
+
+
+def find_card(cards, query):
+    pass
