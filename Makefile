c60ee332712b1f8d01f1ad167adc36c43a8d4f60
hirwa <hirwanshutiflx@gmail.com>
2022-07-24 22:35:17 +0530

    lint fixes

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ install:
 		pip install -r requirements.txt
 
 format:
-	black *.py
+	black $(git ls-files '*.py')
 
 lint:
 	for dir in binary_search_algo.py list.py find_missing_unsorted.py LinearSearch.py tests.py ; do \
--- Sorting/quickSort.py
+++ Sorting/quickSort.py
@@ -38,8 +38,12 @@ def quick_sort(elements, start, end):
 
 
 if __name__ == "__main__":
-    el = [[11, 9, 29, 7, 2, 15, 28], [], [10, 8, 2, 1, 0],
-         [1, 2], [20, 1920, 0, 0]]
+    el = [
+        [11, 9, 29, 7, 2, 15, 28],
+        [], 
+        [10, 8, 2, 1, 0],
+        [1, 2], 
+        [20, 1920, 0, 0]]
     for element in el:
         quick_sort(element, 0, len(element) - 1)
         print(f"The sorted array: {element}")
